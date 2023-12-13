71beb07bc6353ae28013d001fb84ea36524f6de2
HITESH KUMAR <73693548+thehtk@users.noreply.github.com>
2023-12-13 09:11:40 +0530

    Update WAT.py

--- pages/WAT.py
+++ pages/WAT.py
@@ -3,6 +3,7 @@ import random
 import os
 import time
 import csv
+import winsound
 
 
 
@@ -37,7 +38,7 @@ def show(test_words,no_ques):
             
             
             time.sleep(1)
-        #winsound.Beep(500, 300)
+        winsound.Beep(2000, 300)
             
             
     image_placeholder = st.empty()
